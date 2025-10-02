e3f465e25916077a5f451225de743e0f91898828
Ignacio Romero <nachoromero84@hotmail.com>
2025-10-01 22:02:09 -0300

    Add supabase CLI y edge funcitons dev local

---
+++ supabase/functions/getVehiculos/index.ts
@@ -0,0 +1,31 @@
+// supabase/functions/clientes-list/index.ts
+import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
+import { createClient } from 'npm:@supabase/supabase-js@2';
+
+serve(async (req) => {
+try {
+    const supabase = createClient(Deno.env.get('SUPABASE_URL') ?? '', Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '', {
+      global: {
+        headers: {
+          Authorization: req.headers.get('Authorization')
+        }
+      }
+    });
+    const { data, error } = await supabase.from('vehiculos').select();
+    if (error) {
+      throw error;
+    }
+    return new Response(JSON.stringify({
+      data: data
+    }), {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      status: 200
+    });
+  } catch (err) {
+    return new Response(String(err?.message ?? err), {
+      status: 500
+    });
+  }
+});
