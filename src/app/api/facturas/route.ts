9bb93c548e5dda94aa087a423539fdec30faffdf
Ignacio Romero <nachoromero84@hotmail.com>
2025-12-24 00:37:02 -0300

    agrega ArcaSDK como libreria a probar

--- src/app/api/facturas/route.ts
+++ src/app/api/facturas/route.ts
@@ -1,6 +1,12 @@
 import { NextResponse as Response } from "next/server";
-import { AfipServices, IConfigService } from "facturajs";
+//import { AfipServices, IConfigService } from "facturajs";
 import moment from 'moment';
+import { PadronA13Client } from "./padronA13";
+import { readFile } from "node:fs/promises";
+import { resolve } from "node:path";
+import forge from "node-forge";
+
+export const runtime = "nodejs";
 
 const config = {
     certPath: './secret/cert.pem',
@@ -13,12 +19,12 @@ const config = {
     tokensExpireInHours: 12,
 }
 
-const afip = new AfipServices(config);
+//const afip = new AfipServices(config);
 const cuit = 20442094161;
-
+/*
 async function facturaBExample() {
     const res = await afip.getLastBillNumber({
-        Auth: { 
+        Auth: {
             Cuit: cuit,
             Denominacion_Representado: "Agustín Bustos Piasentini",
             Condicion_IVA: "Responsable Inscripto",
@@ -87,11 +93,61 @@ async function facturaBExample() {
     console.log('Created bill', JSON.stringify(resBill, null, 4));
     return resBill;
 }
+*/
+import { Arca } from "@arcasdk/core";
+
+// Instancia la SDK con tus credenciales
+const certPath = resolve(process.cwd(), "secret/prodCred", "B2Car_12cc73e3e1112b55.crt");
+const keyPath = resolve(process.cwd(), "secret", "private_key.key");
+const ticketPath = resolve(process.cwd(), "secret", ".ticket");
+
+const [certPem, keyPem] = await Promise.all([
+    readFile(certPath, "utf8"),
+    readFile(keyPath, "utf8"),
+]);
+
+const cert = forge.pki.certificateFromPem(certPem);
+const key = forge.pki.privateKeyFromPem(keyPem);
+
+const arca = new Arca({
+    production: false, // true para producción, false para homologación
+    cuit: 20442094161, // Tu CUIT (sin guiones)
+    cert, // node-forge certificate
+    key, // node-forge privateKey
+    ticketPath,
+});
+
+async function exampleGetPadron() {
+    const taxpayerDetails = await arca.registerScopeThirteenService.getTaxpayerDetails(
+        27435163713,
+    );
+
+    if (taxpayerDetails) {
+        console.log("Datos del contribuyente:", taxpayerDetails);
+        console.log("Domocilio", taxpayerDetails.datosGenerales?.domicilio);
+    } else {
+        console.log("Contribuyente no encontrado.");
+    }
+    return taxpayerDetails;
+}
+
+async function createNuevaFacturaElectronica() {
+    const voucherInfo = await arca.electronicBillingService.getVoucherInfo(1, 1, 6);
+    
+
+    if (voucherInfo) {
+        console.log("Datos del comprobante:", voucherInfo);
+    } else {
+        console.log("El comprobante no existe.");
+    }
+}
 
 // ESTO SE ESTA LLAMANDO DESDE EL BOTON DE AGREGAR CLIENTE
 export async function POST(req: Request) {
     try {
-        const resBill = await facturaBExample();
+        //const resBill = await facturaBExample();
+        const resBill = await exampleGetPadron();
+        //const resBill = await createNuevaFacturaElectronica();
         return Response.json(resBill);
     }
     catch (error) {
