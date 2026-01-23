3e7e632c60abf07b1caeb9da61738d5cf3b24641
Ignacio Romero <nachoromero84@hotmail.com>
2026-01-22 23:33:29 -0300

    arregla mayusculas

--- src/app/components/turnos/utils/estado.ts
+++ src/app/components/turnos/utils/estado.ts
@@ -4,8 +4,8 @@ import { COLOR } from "@/theme/theme";
 import { TurnoEstado } from "@/model/dtos";
 
 export function estadoAccentColor(estado: TurnoEstado) {
-  if (estado === "Cancelado") return COLOR.ICON.DANGER;
-  if (estado === "Pendiente") return COLOR.BORDER.WEAK;
+  if (estado === "cancelado") return COLOR.ICON.DANGER;
+  if (estado === "pendiente") return COLOR.BORDER.WEAK;
   return COLOR.ACCENT.PRIMARY;
 }
 
