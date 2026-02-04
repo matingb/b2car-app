c918f486b549e8bc23455f359fac4d0aed09f5c2
Ignacio Romero <nachoromero84@hotmail.com>
2026-02-04 10:45:07 -0300

    agrega import provider

--- src/app/(user)/vehiculos/layout.tsx
+++ src/app/(user)/vehiculos/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ClientesProvider } from "@/app/providers/ClientesProvider";
 import { VehiculosProvider } from "@/app/providers/VehiculosProvider";
 import { ArreglosProvider } from "@/app/providers/ArreglosProvider";
+import { InventarioProvider } from "@/app/providers/InventarioProvider";
 
 export default function VehiculosLayout({ children }: { children: React.ReactNode }) {
   return (
