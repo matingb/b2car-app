5eef8ccdabfea68087961f550bd5c4e232683180
Ignacio Romero <nachoromero84@hotmail.com>
2026-03-13 20:34:26 -0300

    borra carpetas supabase al pedo

--- supabase/functions/delete_empresa.sql
+++
@@ -1,9 +0,0 @@
--- Función para eliminar un cliente empresa en una transacción atómica
--- Elimina primero de la tabla empresas y luego de la tabla clientes
-CREATE OR REPLACE FUNCTION delete_empresa(empresa_id uuid)
-RETURNS void AS $$
-BEGIN
-  DELETE FROM empresas WHERE id = empresa_id;
-  DELETE FROM clientes WHERE id = empresa_id;
-END;
-$$ LANGUAGE plpgsql;
