04314bb50bb6f46ee36c2f6895814e2c3a7024d9
Ignacio Romero <nachoromero84@hotmail.com>
2026-03-19 16:10:42 -0300

    Fix agrega provider en detalle vehiculo

--- src/app/(user)/vehiculos/layout.tsx
+++ src/app/(user)/vehiculos/layout.tsx
@@ -3,6 +3,7 @@ import { ClientesProvider } from "@/app/providers/ClientesProvider";
 import { VehiculosProvider } from "@/app/providers/VehiculosProvider";
 import { ArreglosProvider } from "@/app/providers/ArreglosProvider";
 import { InventarioProvider } from "@/app/providers/InventarioProvider";
+import { FormulariosProvider } from "@/app/providers/FormulariosProvider";
 
 export default function VehiculosLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,7 +11,9 @@ export default function VehiculosLayout({ children }: { children: React.ReactNod
       <VehiculosProvider>
         <ArreglosProvider>
           <InventarioProvider>
+            <FormulariosProvider>
             {children}
+            </FormulariosProvider>
           </InventarioProvider>
         </ArreglosProvider>
       </VehiculosProvider>
